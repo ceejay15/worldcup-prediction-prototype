836c2ac3661490a4c7aa42a45b82ef0dbe0c4d9d
charrie15 <joie_pleaides@yahoo.com>
2022-11-22 11:31:04 +0800

    initial commit and push

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/worldcup-prediction-prototype'),
+      dir: require('path').join(__dirname, './coverage/prototype-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'worldcup-prediction-prototype'`, () => {
+  it(`should have as title 'prototype-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('worldcup-prediction-prototype');
+    expect(app.title).toEqual('prototype-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('worldcup-prediction-prototype app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('prototype-app app is running!');
   });
 });
